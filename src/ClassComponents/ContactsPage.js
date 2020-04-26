bbb9ff08b9cf2c221feb134b9132ebba4e996406
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-26 17:05:50 +0530

    contacts list

---
+++ src/ClassComponents/ContactsPage.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { StyleSheet, Text, View, SectionList } from "react-native";
+import { FAB } from "react-native-paper";
+
+import contactsList from "../utils/Contacts";
+import Contact from "./Contact";
+import AddContactForm from "./AddContactForm";
+
+const SectionHeader = (props) => {
+  const sectionHeaderStyles = StyleSheet.create({
+    sectionHeaderText: {
+      fontSize: 22,
+      fontWeight: "bold",
+      paddingLeft: 20,
+      color: "#1d6ccc",
+    },
+    sectionHeader: {
+      borderBottomColor: "#aaa",
+      borderBottomWidth: 1,
+      marginBottom: 4,
+      //   padding: 10,
+    },
+  });
+  return (
+    <View style={sectionHeaderStyles.sectionHeader}>
+      <Text style={sectionHeaderStyles.sectionHeaderText}>{props.title}</Text>
+    </View>
+  );
+};
+
+export default class ContactsPage extends React.Component {
+  state = {
+    showAddContactsForm: false,
+    contacts: contactsList,
+  };
+  constructor(props) {
+    super(props);
+    contactsList.sort((a, b) => a.name > b.name);
+    const data = contactsList.reduce((obj, contact) => {
+      const firstLetter = contact.name[0].toUpperCase();
+      return {
+        ...obj,
+        [firstLetter]: [...(obj[firstLetter] || []), contact],
+      };
+    }, {});
+
+    this.sections = Object.keys(data)
+      .sort()
+      .map((letter) => ({ title: letter, data: data[letter] }));
+  }
+
+  renderItem = ({ item }) => <Contact {...item} />;
+
+  renderSectionHeader = ({ section: { title } }) => (
+    <SectionHeader title={title} />
+  );
+
+  showAddContactsForm = () => {
+    this.setState((prevState) => ({
+      showAddContactsForm: true,
+    }));
+  };
+  hideAddContactsForm = () => {
+    this.setState((prevState) => ({
+      showAddContactsForm: false,
+    }));
+  };
+
+  handleAddContact = (contact) => {
+    this.hideAddContactsForm();
+    //   console.log(contact);
+    const firstLetter = contact.firstName[0].toUpperCase();
+    const contactObj = {
+      name: `${contact.firstName} ${contact.lastName}`,
+      phone: contact.phone,
+      key: contactsList.length,
+    };
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.title == firstLetter
+    );
+    if (sectionIndex > -1) {
+      this.sections[sectionIndex].data = [
+        ...this.sections[sectionIndex].data,
+        contactObj,
+      ];
+      this.sections[sectionIndex].data.sort((a, b) => {
+        console.log(`${a.name} ${b.name} ${a.name > b.name}`);
+        return a.name > b.name;
+      });
+      this.sections[sectionIndex].data.map((data) => console.log(data));
+    } else {
+      this.sections = [
+        ...this.sections,
+        { title: firstLetter, data: [contactObj] },
+      ];
+      this.sections.sort((a, b) => a.title > b.title);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        {this.state.showAddContactsForm ? (
+          <AddContactForm
+            cancel={this.hideAddContactsForm}
+            save={this.handleAddContact}
+          />
+        ) : (
+          <View style={{ height: "100%", marginTop: 10 }}>
+            <SectionList
+              style={{
+                paddingHorizontal: 10,
+                marginBottom: 0,
+              }}
+              sections={this.sections}
+              renderItem={this.renderItem}
+              renderSectionHeader={this.renderSectionHeader}
+            />
+            <FAB
+              style={styles.fab}
+              icon="plus"
+              onPress={this.showAddContactsForm}
+            />
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    zIndex: 1,
+    margin: 16,
+    right: 5,
+    bottom: 30,
+    backgroundColor: "#1d6ccc",
+  },
+});
---
+++ src/utils/nameList.js
@@ -0,0 +1,202 @@
+export default names = [
+  {
+    firstName: "Sweeney",
+    lastName: "Kerr",
+  },
+  {
+    firstName: "Silvia",
+    lastName: "Peterson",
+  },
+  {
+    firstName: "Young",
+    lastName: "Gay",
+  },
+  {
+    firstName: "Georgette",
+    lastName: "Osborn",
+  },
+  {
+    firstName: "Barrett",
+    lastName: "Schmidt",
+  },
+  {
+    firstName: "Huff",
+    lastName: "Burks",
+  },
+  {
+    firstName: "Bush",
+    lastName: "Wilkerson",
+  },
+  {
+    firstName: "Soto",
+    lastName: "Collier",
+  },
+  {
+    firstName: "Rosalinda",
+    lastName: "Horne",
+  },
+  {
+    firstName: "Kristy",
+    lastName: "Hamilton",
+  },
+  {
+    firstName: "Bette",
+    lastName: "Wyatt",
+  },
+  {
+    firstName: "Adrian",
+    lastName: "Curtis",
+  },
+  {
+    firstName: "Hensley",
+    lastName: "Simmons",
+  },
+  {
+    firstName: "Bishop",
+    lastName: "Mccormick",
+  },
+  {
+    firstName: "Pamela",
+    lastName: "Hall",
+  },
+  {
+    firstName: "Rodriguez",
+    lastName: "Perez",
+  },
+  {
+    firstName: "Mccarthy",
+    lastName: "Bolton",
+  },
+  {
+    firstName: "Patrice",
+    lastName: "Ruiz",
+  },
+  {
+    firstName: "Shelley",
+    lastName: "Glass",
+  },
+  {
+    firstName: "Lorene",
+    lastName: "Conrad",
+  },
+  {
+    firstName: "Faye",
+    lastName: "Foreman",
+  },
+  {
+    firstName: "Tonya",
+    lastName: "Reynolds",
+  },
+  {
+    firstName: "Coleen",
+    lastName: "Sweet",
+  },
+  {
+    firstName: "Rose",
+    lastName: "Castillo",
+  },
+  {
+    firstName: "Rosario",
+    lastName: "Sparks",
+  },
+  {
+    firstName: "Stein",
+    lastName: "Allen",
+  },
+  {
+    firstName: "Amy",
+    lastName: "Farmer",
+  },
+  {
+    firstName: "Bean",
+    lastName: "Merritt",
+  },
+  {
+    firstName: "Betty",
+    lastName: "Melendez",
+  },
+  {
+    firstName: "Shauna",
+    lastName: "Zamora",
+  },
+  {
+    firstName: "Ramos",
+    lastName: "Franklin",
+  },
+  {
+    firstName: "Dixon",
+    lastName: "Stanton",
+  },
+  {
+    firstName: "Graham",
+    lastName: "Harvey",
+  },
+  {
+    firstName: "Acevedo",
+    lastName: "Finley",
+  },
+  {
+    firstName: "Russell",
+    lastName: "Bates",
+  },
+  {
+    firstName: "Ivy",
+    lastName: "Levine",
+  },
+  {
+    firstName: "Winnie",
+    lastName: "Mcdonald",
+  },
+  {
+    firstName: "Kari",
+    lastName: "Holt",
+  },
+  {
+    firstName: "Adele",
+    lastName: "Branch",
+  },
+  {
+    firstName: "Gibson",
+    lastName: "Gallagher",
+  },
+  {
+    firstName: "Wilkerson",
+    lastName: "Mitchell",
+  },
+  {
+    firstName: "Gay",
+    lastName: "Pollard",
+  },
+  {
+    firstName: "Conner",
+    lastName: "Clarke",
+  },
+  {
+    firstName: "Debra",
+    lastName: "Cotton",
+  },
+  {
+    firstName: "Gwen",
+    lastName: "Moon",
+  },
+  {
+    firstName: "Sophie",
+    lastName: "Vargas",
+  },
+  {
+    firstName: "Moses",
+    lastName: "Cummings",
+  },
+  {
+    firstName: "Jacobson",
+    lastName: "Ratliff",
+  },
+  {
+    firstName: "Townsend",
+    lastName: "Coffey",
+  },
+  {
+    firstName: "Natasha",
+    lastName: "Fischer",
+  },
+];
