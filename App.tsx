7bfb06aecd25f32fc75272e6c689bd6a83961f4a
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-26 17:05:15 +0530

    set up app navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import { Button, BottomNavigation, Text } from "react-native-paper";
+import ContactsPage from "./src/ClassComponents/ContactsPage";
+import Contact from "./src/ClassComponents/Contact";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+const ContactsRoute = () => <ContactsPage />;
+const callLogRoute = () => <Text>Call Logs page</Text>;
+const PhoneScreenRoute = () => <Text>Phone Screen Route</Text>;
+
+export default class App extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      {
+        key: "callLog",
+        title: "",
+        icon: "history",
+        badge: false,
+      },
+      {
+        key: "dialPad",
+        title: "",
+        icon: "dialpad",
+        badge: false,
+      },
+      {
+        key: "contacts",
+        title: "",
+        icon: "account",
+        badge: false,
+      },
+    ],
+  };
+
+  _handleIndexChange = (index) => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    callLog: callLogRoute,
+    dialPad: PhoneScreenRoute,
+    contacts: ContactsRoute,
+  });
+
+  render() {
+    return (
+      <BottomNavigation
+        shifting
+        labeled={false}
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+        barStyle={{
+          backgroundColor: "#1d6ccc",
+        }}
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
