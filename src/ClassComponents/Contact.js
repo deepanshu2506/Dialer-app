e4069e5884c1ccc923ac47b8c95844c794ac6263
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-27 19:07:11 +0530

    UI tweaks to each contact

--- src/ClassComponents/Contact.js
+++ src/ClassComponents/Contact.js
@@ -1,24 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableNativeFeedback } from "react-native";
 import { Avatar } from "react-native-paper";
 
 import { primaryColor } from "../../AppStyles";
 
 export default class Contact extends React.Component {
   state = { expanded: false };
+
+  componentDidMount() {
+    const { firstName, lastName, namePrefix } = this.props;
+    let avatarText = "",
+      name = "";
+    if (namePrefix) {
+      name += namePrefix + " ";
+    }
+
+    if (firstName) {
+      avatarText += firstName[0];
+      name += firstName + " ";
+    }
+    if (lastName) {
+      avatarText += lastName[0];
+      name += lastName + " ";
+    }
+    this.setState({ avatarText, name });
+  }
   render() {
-    const [firstName, lastName] = this.props.name.split(" ");
     return (
       <View style={styles.contactCard}>
         <Avatar.Text
           size={40}
           style={styles.Avatar}
-          label={`${firstName[0].toUpperCase()}${lastName[0].toUpperCase()}`}
+          label={this.state.avatarText}
         />
-        <View>
-          <Text style={styles.nameText}>{this.props.name}</Text>
-          <Text style={styles.phoneText}>{this.props.phone}</Text>
-        </View>
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple("#aaa")}
+          onPress={() => {
+            console.log(this.props);
+          }}
+        >
+          <View style={styles.touchable}>
+            <Text style={styles.nameText}>{this.state.name}</Text>
+            {this.props.company ? (
+              <Text style={styles.phoneText}>{this.props.company}</Text>
+            ) : null}
+          </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
@@ -27,11 +54,15 @@ export default class Contact extends React.Component {
 const styles = StyleSheet.create({
   contactCard: {
     padding: 15,
+    paddingRight: 0,
+    paddingVertical: 0,
     flexDirection: "row",
     alignItems: "center",
+    // borderWidth: 1,
+    height: 70,
   },
   Avatar: {
-    marginRight: 20,
+    marginRight: 0,
     backgroundColor: primaryColor,
   },
   nameText: {
@@ -39,5 +70,11 @@ const styles = StyleSheet.create({
   },
   phoneText: {
     fontSize: 12,
+  },
+  touchable: {
+    paddingLeft: 20,
+    flexGrow: 1,
+    height: 70,
+    justifyContent: "center",
   },
 });
