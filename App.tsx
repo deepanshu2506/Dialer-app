99502c344172ac336cf28d4b838e926fb338d786
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-04-28 20:50:10 +0530

    hadling permissions

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { View } from "react-native";
+import { View, PermissionsAndroid, Platform } from "react-native";
 import { Button, BottomNavigation, Text } from "react-native-paper";
 import ContactsPage from "./src/ClassComponents/ContactsPage";
 import DialerPage from "./src/ClassComponents/DialerPage";
-
-const ContactsRoute = () => <ContactsPage />;
-const callLogRoute = () => <Text>Call Logs page</Text>;
-const PhoneScreenRoute = () => <DialerPage />;
+import ContactStore from "./src/utils/ContactsStore";
 
 export default class App extends React.Component {
+  ContactsRoute = () => <ContactsPage permissions={this.state.permissions} />;
+  callLogRoute = () => <Text>Call Logs page</Text>;
+  PhoneScreenRoute = () => {
+    console.log(this.state);
+    return (
+      <DialerPage
+        permissions={this.state.permissions["android.permission.CALL_PHONE"]}
+      />
+    );
+  };
+
   state = {
     index: 0,
     routes: [
@@ -31,14 +39,33 @@ export default class App extends React.Component {
         badge: false,
       },
     ],
+    permissions: {},
   };
+  componentDidMount() {
+    if (Platform.OS === "android") {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS,
+        PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+      ])
+        .then((result) => {
+          this.setState({ permissions: result });
+          if (result["android.permission.READ_CONTACTS"] == "granted") {
+            ContactStore.init();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
 
   _handleIndexChange = (index) => this.setState({ index });
 
   _renderScene = BottomNavigation.SceneMap({
-    callLog: callLogRoute,
-    dialPad: PhoneScreenRoute,
-    contacts: ContactsRoute,
+    callLog: this.callLogRoute,
+    dialPad: this.PhoneScreenRoute,
+    contacts: this.ContactsRoute,
   });
 
   render() {
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
+import React from "react";
+import { registerRootComponent } from "expo";
+import { Provider } from "react-native-paper";
 
-import App from './App';
+import App from "./App";
 
+root = (props) => (
+  <Provider>
+    <App />
+  </Provider>
+);
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(root);
